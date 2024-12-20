3cb41f7798bb530fd2f3dfba1745e4a8b0786e52
Chih Wang <ongchi@users.noreply.github.com>
2024-12-20 21:40:45 +0800

    fix: update ex1

--- examples/ex1.rs
+++ examples/ex1.rs
@@ -3,11 +3,13 @@ use std::env;
 use lagrit_bindings::{InitMode, LaGriT, LagritError};
 
 fn main() -> Result<(), LagritError> {
-    let workdir = format!("{}/examples/ex1_output", env!["CARGO_MANIFEST_DIR"]);
-    std::fs::create_dir_all(&workdir).unwrap();
-    env::set_current_dir(workdir).unwrap();
-
-    let lg = LaGriT::new(InitMode::Slient, None, None)?;
+    let workdir = format!("{}/examples/output", env!["CARGO_MANIFEST_DIR"]);
+    let lg = LaGriT::new(
+        InitMode::Slient,
+        Some("ex1.log"),
+        Some("ex1.out"),
+        Some(&workdir),
+    )?;
 
     lg.sendcmd("cmo/create/mocyl/ / /tet")?;
     lg.sendcmd("createpts/rtz/3,4,2/0.0,0.0,0.0/6.0,360.0,2.0/1,1,1")?;
@@ -15,7 +17,7 @@ fn main() -> Result<(), LagritError> {
     lg.sendcmd("pset/p2/attribute/xic/1,0,0/get 1.0")?;
     lg.sendcmd("pset/p3/attribute/xic/1,0,0/get 1.0")?;
     lg.sendcmd("connect/noadd")?;
-    lg.sendcmd("dump/avs/test.inp/mocyl")?;
+    lg.sendcmd("dump/avs/ex1_test.inp/mocyl")?;
 
     let cmo = lg.cmo()?;
     println!("cmo: {}", cmo.name());
